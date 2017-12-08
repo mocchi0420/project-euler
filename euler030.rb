f3e3dd079a8ff5a2ece8f330dc42ffdfb5bf526c
suzuki motoharu <suzuki@toydea.com>
2017-12-08 17:52:08 +0900

    euler30

--- euler030.rb
+++ euler030.rb
@@ -2,53 +2,45 @@
 # Problem 30 「各桁の5乗」
 
 # 簡単な方針
-#
-# 各項の5乗の和をSと置く
-#
-# このとき、Sの数値の範囲から考えてみる。
-# 10^0 <= S <= 10^1-1 :1桁の数値の5乗で1桁の数値(ただし1を除く)を表す
-# 10^1 <= S <= 10^2-1 :2桁の数値の各項の5乗の和で2桁の数値を表す
-# 10^2 <= S <= 10^3-1 :3桁の数値の各項の5乗の和で3桁の数値を表す
-# ---(中略)---
-# 10^(n-1) から 10^n-1 :n桁の数値の各項の5乗の和でn桁の数値を表す
-# 
-# また、9^5=59049であることから、n桁の数値がすべて9で埋め尽くされていても、
-# その場合の数値はたかだかn*9^5である。よって、あるnを超えた時に、n*9^5 < 10^(n-1)となり、
-# 絶対にn桁の数値を表すことができなくなる。ここをループの終端として考えると、
-#
-# n*9^5 >= 10^(n-1) <=> n*9^5 - 10^(n-1) >= 0
-#
-# これはもう計算機を回したほうが早いのでこのnを基準にループを回す。(回したらn=6でした。)
-#
-# さて、ここからが本題。
-#
+# とにかくめちゃくちゃ高速化には成功したので、リファクタは後でやる
+
+
 
 require 'prime'
 
 def euler030
-	loop_num = 2	# 最大ループ回数
-	fp_arr = (0..9).map{|d| d.to_i**5}
-	ret = []
 
-	2.upto(loop_num) do |data|
-	
-		10**(data-1).upto(10**data-1) do |idx|
-			tmp = idx.to_s.split("").map{|d| fp_arr[d.to_i]}.inject(:+)
-			next if Math.log10(tmp).floor == data
-			p "data: #{data}, idx: #{idx}, tmp: #{tmp}, log: #{Math.log(tmp,10).floor}"
-		end
-	
-=begin	
-		index = 10**(data)
-		tmp_list = (0..9).to_a.select{|d| index > d**5}
-		tmp_list.combination(data).each do |d|
-			ans = d.map{|e| e**5}.inject(:+)
+	start_time = Time.now
 
-			ret << ans if ans.to_s.split("").sort.map{|e| e.to_i} == d.sort 
-			#ret << ans if ans.to_s.split("").sort.map{|e| e.to_i} == d.map{|e| e.to_s}.sort
+	hoge = (0..9).to_a.map{|d| [d]}
+	1.upto(5) do |idx|
+		tmp = []
+		hoge.each do |d|
+			0.upto(9) do |num|
+				if d.max <= num
+					t_ary = Marshal.load(Marshal.dump(d))
+					t_ary << num
+					tmp << t_ary.sort
+				end
+			end
 		end
-=end
+		hoge = tmp
 	end
+	hoge = hoge.uniq
+	
+	ans_arr = hoge.map{|d| d.map{|e| e**5}.inject(:+)}
+	
+	loop_num = 6*9**5	# 最大ループ回数
 
+	ret = []
+	
+	ans_arr.each_with_index do |data, idx|
+		tmp_data = data.to_s.split("").map{|d| d.to_i}
+		if tmp_data.length < 6 then
+			(6-tmp_data.length).times{tmp_data << 0}
+		end
+		ret << data if hoge[idx] == tmp_data.sort && hoge[idx] != [0,0,0,0,0,1]
+	end
+	
 	return ret.inject(:+)
 end
