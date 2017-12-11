3902deab7e6bc3b0deebf366bfcaa93265b5daa9
suzuki motoharu <suzuki@toydea.com>
2017-12-11 12:47:50 +0900

    euler002_retake

--- euler002.rb
+++ euler002.rb
@@ -1,5 +1,18 @@
 # coding: utf-8
 # Problem 2 「偶数のフィボナッチ数」
+
+def euler002(threshold = 4000000)
+	memo=[0,1,1]
+	ret = 0
+	memo.length.step do |tmp|
+		memo[tmp] = memo[tmp-1]+memo[tmp-2]
+		break if memo[tmp] > threshold
+		ret += memo[tmp] if tmp%3==0
+	end
+	return ret
+end
+
+=begin
 def euler002(threshold = 4000000)
 	ret = 0
 	1.step do |tmp|
@@ -15,3 +28,4 @@ def calc_fib(n, a=0, b=1)
 	return a if n < 1
 	calc_fib(n-1, b, a+b)
 end
+=end
